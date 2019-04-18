22a4e450dc26a71903001379dc1bcb097ac56363
Владимир Реутов <r3utov@gmail.com>
2019-04-19 00:33:40 +1100

    Создана модель представления для формы ввода данных

---
+++ WebSite/Models/UserInputViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebSite.Models
+{
+	public class UserInputViewModel
+	{
+		[Required]
+		[DataType(DataType.Text)]
+		[Display(Name = "Слово")]
+		public string Word { get; set; }
+
+		[Required]
+		[Display(Name = "Действие")]
+		public bool IsActionChecked { get; set; }
+	}
+}
