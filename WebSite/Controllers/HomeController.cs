cd19ea265377d6fcf7b1a085c826fdd07ab2da47
Владимир Реутов <r3utov@gmail.com>
2019-04-19 00:46:14 +1100

    В контроллер добавлены свойства для чтения адресов сервисов из конфига

--- WebSite/Controllers/HomeController.cs
+++ WebSite/Controllers/HomeController.cs
@@ -2,29 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.Mvc;
 
 namespace WebSite.Controllers
 {
 	public class HomeController : Controller
 	{
+		public string MicroService1Address
+		{
+			get
+			{
+				return WebConfigurationManager.AppSettings["MicroService1Address"];
+			}
+		}
+
+		public string MicroService2Address
+		{
+			get
+			{
+				return WebConfigurationManager.AppSettings["MicroService2Address"];
+			}
+		}
+
 		public ActionResult Index()
 		{
 			return View();
 		}
 
-		public ActionResult About()
-		{
-			ViewBag.Message = "Your application description page.";
-
-			return View();
-		}
-
-		public ActionResult Contact()
-		{
-			ViewBag.Message = "Your contact page.";
-
-			return View();
-		}
 	}
 }
