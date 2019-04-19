578b40d05a668d404386cec110452707fe087dd0
Владимир Реутов <r3utov@gmail.com>
2019-04-19 21:59:36 +1100

    Исключены лишние скрипты и стили из конфигурации Bundles

--- WebSite/App_Start/BundleConfig.cs
+++ WebSite/App_Start/BundleConfig.cs
@@ -7,7 +7,7 @@ namespace WebSite
 	{
 		// Дополнительные сведения об объединении см. на странице https://go.microsoft.com/fwlink/?LinkId=301862
 		public static void RegisterBundles(BundleCollection bundles)
-		{
+		{/*
 			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
 						"~/Scripts/jquery-{version}.js"));
 
@@ -21,10 +21,10 @@ namespace WebSite
 
 			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
 					  "~/Scripts/bootstrap.js"));
-
+			*/
 			bundles.Add(new StyleBundle("~/Content/css").Include(
-					  "~/Content/bootstrap.css",
-					  "~/Content/site.css"));
+				//	  "~/Content/bootstrap.css",
+					  "~/Content/Site.css"));
 		}
 	}
 }
