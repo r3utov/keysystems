22d9dbce8aaea5b18c14f6514d980e09b45b9815
Владимир Реутов <r3utov@gmail.com>
2019-04-19 21:11:29 +1100

    Реализованы Api-методы для приема данных и возврата обработанных значений

--- MicroService1/Controllers/ConvertController.cs
+++ MicroService1/Controllers/ConvertController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Web;
+using System.Web.Http;
 
 namespace MicroService1.Controllers
 {
@@ -22,5 +25,22 @@ namespace MicroService1.Controllers
 			return Convert.ToBase64String(inputBytes);
 		}
 
+
+		/// -------------------------------------------------------------- 
+		/// Методы запросов
+		/// --------------------------------------------------------------
+
+		// POST api/<controller>
+		public string Post([FromBody]string input)
+		{
+			/// Проверка строки на стороне сервиса
+			if (string.IsNullOrEmpty(input) == true) {
+				throw new HttpResponseException(
+					new HttpResponseMessage(HttpStatusCode.BadRequest) {
+						Content = new StringContent("Передана пустая строка", Encoding.UTF8)
+					});
+			}
+			return ConvertText(input);
+		}
 	}
 }
--- MicroService2/Controllers/ConvertController.cs
+++ MicroService2/Controllers/ConvertController.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
 using System.Web;
+using System.Web.Http;
 
 namespace MicroService2.Controllers
 {
@@ -18,6 +22,23 @@ namespace MicroService2.Controllers
 		protected string ConvertText(string input)
 		{
 			return String.Join("", input.Reverse());
+		}
+
+		/// -------------------------------------------------------------- 
+		/// Методы запросов
+		/// --------------------------------------------------------------
+
+		// POST api/<controller>
+		public string Post([FromBody]string input)
+		{
+			/// Проверка строки на стороне сервиса
+			if (string.IsNullOrEmpty(input) == true) {
+				throw new HttpResponseException(
+					new HttpResponseMessage(HttpStatusCode.BadRequest) {
+						Content = new StringContent("Передана пустая строка", Encoding.UTF8)
+					});
+			}
+			return ConvertText(input);
 		}
 	}
 }
