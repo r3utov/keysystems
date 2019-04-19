3a1778a2b089dc33d4b047b5b1527683b75192b9
Владимир Реутов <r3utov@gmail.com>
2019-04-19 21:09:04 +1100

    Добавлены контроллеры, реализованы методы для конвертирования текста

---
+++ MicroService1/Controllers/ConvertController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace MicroService1.Controllers
+{
+	public class ConvertController
+	{
+
+		/// -------------------------------------------------------------- 
+		/// Обработка текста
+		/// --------------------------------------------------------------
+
+		/// <summary>
+		/// Конвертирует текст в base64
+		/// </summary>
+		protected string ConvertText(string input)
+		{
+			byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+			return Convert.ToBase64String(inputBytes);
+		}
+
+	}
+}
---
+++ MicroService2/Controllers/ConvertController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MicroService2.Controllers
+{
+	public class ConvertController
+	{
+
+		/// -------------------------------------------------------------- 
+		/// Обработка текста
+		/// --------------------------------------------------------------
+
+		/// <summary>
+		/// Инвертирует строку
+		/// </summary>
+		protected string ConvertText(string input)
+		{
+			return String.Join("", input.Reverse());
+		}
+	}
+}
